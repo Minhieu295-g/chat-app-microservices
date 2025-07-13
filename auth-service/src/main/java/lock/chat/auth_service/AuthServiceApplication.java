614e3ba908b2aca0f9573b25b5acdf154b741337
PeterPann295 <21130354@st.hcmuaf.edu.vn>
2025-07-13 21:15:50 +0700

    Update: auth-service setup-db

--- auth-service/src/main/java/lock/chat/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/lock/chat/auth_service/AuthServiceApplication.java
@@ -1,5 +1,4 @@
 package lock.chat.auth_service;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ auth-service/src/main/java/lock/chat/auth_service/model/AbsEntity.java
@@ -0,0 +1,32 @@
+package lock.chat.auth_service.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class AbsEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Date updatedAt;
+
+}
