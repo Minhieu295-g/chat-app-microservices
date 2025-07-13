4aefde122b8fd1d0b0049cae60d2bf559e08d3e5
PeterPann295 <21130354@st.hcmuaf.edu.vn>
2025-07-13 21:16:59 +0700

    Update: auth-service setup-db

--- auth-service/src/main/java/lock/chat/auth_service/model/AbsEntity.java
+++ auth-service/src/main/java/lock/chat/auth_service/model/AbsEntity.java
@@ -4,12 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.UUID;
 
 @Getter
 @Setter
